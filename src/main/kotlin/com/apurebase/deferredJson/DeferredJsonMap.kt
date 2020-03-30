17b349b5ef2b73fe3ac35864d4bbb86a8a5e0936
Jógvan Olsen <jol@apurebase.com>
2020-03-30 09:12:28 +0100

    Fixed function sometimes returning before finished

--- src/main/kotlin/com/apurebase/deferredJson/DeferredJsonMap.kt
+++ src/main/kotlin/com/apurebase/deferredJson/DeferredJsonMap.kt
@@ -3,50 +3,40 @@ package com.apurebase.deferredJson
 import kotlinx.coroutines.*
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
+import kotlin.coroutines.CoroutineContext
 
 class DeferredJsonMap internal constructor(
-    private val dispatcher: CoroutineDispatcher,
-    val level: Int = 0
+    ctx: CoroutineContext
 ): CoroutineScope {
 
     internal val job = Job()
-    override val coroutineContext = (dispatcher + job)
+    override val coroutineContext = ctx + job
 
     private val deferredMap = mutableMapOf<String, Deferred<JsonElement>>()
     private var completedMap: Map<String, JsonElement>? = null
 
-
-    private fun log(msg: String) {
-        println("[DeferredJsonMap-$level]: $msg")
-    }
-
     infix fun String.toValue(element: JsonElement) {
-        log("toValue")
         this toDeferredValue CompletableDeferred(element)
     }
 
     infix fun String.toDeferredValue(element: Deferred<JsonElement>) {
-        log("toDeferredValue")
         deferredMap[this] = element
     }
 
     suspend infix fun String.toDeferredObj(block: suspend DeferredJsonMap.() -> Unit) {
-        log("toDeferredObj")
-        val map = DeferredJsonMap(dispatcher, level + 1)
+        val map = DeferredJsonMap(coroutineContext)
         block(map)
         this@toDeferredObj toDeferredValue map.asDeferred()
     }
 
     suspend infix fun String.toDeferredArray(block: suspend DeferredJsonArray.() -> Unit) {
-        log("toDeferredArray")
-        val array = DeferredJsonArray(dispatcher)
+        val array = DeferredJsonArray(coroutineContext)
         block(array)
         this@toDeferredArray toDeferredValue array.asDeferred()
     }
 
     @Suppress("DeferredIsResult")
     fun asDeferred(): Deferred<JsonElement> {
-        log("asDeferred")
         return async(coroutineContext, start = CoroutineStart.LAZY) {
             awaitAll()
             build()
@@ -54,17 +44,14 @@ class DeferredJsonMap internal constructor(
     }
 
     suspend fun awaitAll() {
-        log("awaitAll")
         check(completedMap == null) { "The deferred tree has already been awaited!" }
         completedMap = deferredMap.mapValues { it.value.await() }
 
-        log("awaitAll:done")
 
         job.complete()
     }
 
     fun build(): JsonObject {
-        log("build")
         checkNotNull(completedMap) { "The deferred tree has not been awaited!" }
         return JsonObject(completedMap!!)
     }
